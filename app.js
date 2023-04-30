04dda6d020279f007951f6b63d2b2cd081c1f023
NidhiItcode <nidhi.makdani.itcodehelp@gmail.com>
2023-04-30 22:33:59 +0530

    xyz

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ mongoose.connect(DATABASE_URL, {
 }); // connect to DATABASE_URL
 console.log("MongoDb Connection: ", DATABASE_URL);
 
-// CONNECTION EVENTS
-// When successfully connected
-mongoose.connection.on("connected", function () {
-  console.log("Mongoose default connection open to " + DATABASE_URL);
-});
+// // CONNECTION EVENTS
+// // When successfully connected
+// mongoose.connection.on("connected", function () {
+//   console.log("Mongoose default connection open to " + DATABASE_URL);
+// });
 
-// If the connection throws an error
-mongoose.connection.on("error", function (err) {
-  console.log("Mongoose default connection error: " + err);
-});
+// // If the connection throws an error
+// mongoose.connection.on("error", function (err) {
+//   console.log("Mongoose default connection error: " + err);
+// });
 
-// When the connection is disconnected --
-mongoose.connection.on("disconnected", function () {
-  console.log("Mongoose default connection disconnected");
-});
+// // When the connection is disconnected --
+// mongoose.connection.on("disconnected", function () {
+//   console.log("Mongoose default connection disconnected");
+// });
 
 // Body-parser
 app.use(cors({origin: '*'}));
